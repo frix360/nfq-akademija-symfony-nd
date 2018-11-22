0c8b0df32e320df8ebf128e81e1cafd9a64b515f
Ignas Dailydė <ignas.dailyde@gmail.com>
2018-11-23 00:19:58 +0200

    Namų darbas no1

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,12 +8,42 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class HomeController extends Controller
 {
     /**
-     * @Route("/", name="home")
+     * @Route("/", name="students.index")
      */
     public function index()
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        $dataJson = file_get_contents(__DIR__ . '/../../public/students.json', FILE_USE_INCLUDE_PATH);
+
+        $data = json_decode($dataJson);
+        $students = [];
+        $projects = [];
+
+        foreach ($data as $key => $value) {
+            $projects[] = [
+                'short_title' => $key,
+                'full_title' => $value->name,
+                'github_link' => 'github.com/nfqakademija/' . $key,
+                'web_link' => $key . '.projektai.nfqakademija.lt/ ',
+                'ssh' => 'ssh.' .  $key . '@deploy.nfqakademija.lt -p 2222'
+            ];
+
+            foreach ($value->students as $student) {
+                $students[] = [
+                    'name' => $student,
+                    'project' => $key,
+                    'mentors' => $value->mentors
+                ];
+            }
+        }
+
+        return $this->render('students/index.html.twig', compact('students', 'projects'));
+    }
+
+    /**
+     * @Route("/student", name="students.show")
+     */
+    public function show() {
+
+        return $this->render('students/show.html.twig');
     }
 }
